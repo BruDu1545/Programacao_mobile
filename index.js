ec4608fd3c38a59a8df2c80577158726eb02ac86
Bruno Carletto Adamczyk <150372999+BruDu1545@users.noreply.github.com>
2024-02-21 18:07:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+let idade = 15;
+let nome = 'Bruno';
+let identidade_de_maior = false;
+let num_cnh = null;
+let nome_da_mae = undefined;
+let chave_ativacao = Symbol('ativacao');
+
+
+function Function1(){
+console.log(10 == 10);
+}
+
+
+
