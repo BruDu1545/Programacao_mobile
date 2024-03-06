a4f53fc9b5f5beecadb4771ee58d81fa0611528e
Bruno Carletto Adamczyk <150372999+BruDu1545@users.noreply.github.com>
2024-03-06 18:00:47 -0300

    Create Code.dart

---
+++ Code.dart
@@ -0,0 +1,47 @@
+void main() {
+    List <int> idades = [14, 20, 13, 42, 54, 06];
+    List <String> nome = ['Bruno', 'Marcio', 'Jhuan', 'henrique', 'Cleyton', 'Joao'];
+    List <String> profissao = ['Programadoe', '20', '13', '42', '54', '06'];
+  
+  
+  
+  
+  /*
+  String nome1 = 'macaco do marcio';
+  String nome2 = 'Bruno';
+  print(nome1 == nome2);
+  print(nome1 != nome2);
+  
+  print('----------');
+  
+  int num1 = 69;
+  int num2 = 96;
+  print(num1 == num2);
+  print(num1 != num2);
+  
+  print('----------');
+  
+  double samsung = 19;
+  double xiaomi = 100;
+  double lg = -1;
+  double motorola = 2;
+  if (xiaomi > samsung && lg > motorola){   
+  print('O xiaomi é verdadeiro');
+  } else {
+  print('O xiaomi é falso ');
+  }
+  */
+  /*
+  int num1 = 69;
+  int num2 = 96;
+  
+  print(num1 == num2);
+  print(num1 != num2);
+  */
+  /*
+  String nome = 'Bruno';
+  int idade = 16;
+  double altura = 1.90;
+  
+  print('Sr. ${nome} vc possui ${idade} anos, e tem ${altura}m de altura.');*/
+}
